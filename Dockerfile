ad865c1220acfe8752f06fcc51376bb1244873da
DongvinPark <dongvin99@naver.com>
2023-07-31 15:29:07 +0900

    스프링부트 컨테이너에서 로그 저장하는 기능 추가

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:17
 ARG JAR_FILE=build/libs/app.jar
 COPY ${JAR_FILE} ./app.jar
+ADD src/main/resources/logback-prod.xml ./src/main/resources/logback-prod.xml
 ENV TZ=Asia/Seoul
 ENTRYPOINT ["java", "-jar", "./app.jar"]
--- src/main/java/com/example/demo/persist/Controller.java
+++ src/main/java/com/example/demo/persist/Controller.java
@@ -1,22 +1,26 @@
 package com.example.demo.persist;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class Controller {
 
   private final TestRepository testRepository;
 
   @PostMapping("/test")
   public TestEntity test(){
-    return testRepository.save(
+    TestEntity testEntity =testRepository.save(
         TestEntity.builder()
             .name("test")
             .build()
     );
+    log.info("### controller.test : TestEntity.entityId {} saved!", testEntity.getId());
+    return testEntity;
   }
 
 }
