dfa955fde857a774ad059fb5242ec4d8c4cf63c7
DongvinPark <dongvin99@naver.com>
2023-08-10 11:34:08 +0900

    S3에 업로드 하는 방식 변경

--- src/main/java/com/example/demo/service/AmazonS3Service.java
+++ src/main/java/com/example/demo/service/AmazonS3Service.java
@@ -28,28 +28,23 @@ public class AmazonS3Service {
 
     try {
 
-      // 로그 파일을 찾아야 한다. 여러 개의 로그 파일중 일단 1개의 파일만 찾아본다.
-      // 이미 s3에 기록된 로그 파일은 삭제해서 파일의 종복을 막는 방법도 있고,
-      // aws cloud watch의 내용을 바로 s3에 전달하는 기능도 aws에서 제공한다고 한다.
-      // 실제로 이 기능을 쓴다면, 이 작업은 task scheduler를 도입해야 하고 로그 파일들 중
-      // 이미 작성이 끝나서 더 이상 수정할 필요가 없는 가장 오래된 파일을 s3에 업로드 해야할 듯 하다.
-      List<String> lines = Files.readAllLines(Paths.get("./vr3_logs/vr3.log"));
-      StringBuilder sb = new StringBuilder();
-      for(String line : lines){
-        sb.append(line);
-        sb.append("\n");
-      }
+      // 스케줄링 스레드가 필요하다.
+      // ./vr3_logs/라는 디렉토리에서 확장자가 .txt인 로그파일들을 전부 s3에 업로드 한다.
+      // S3에 업로드 완료된 파일은 컨테이너 내부에서 삭제한다.
+      File dir = new File("./vr3_logs");
+      String[] fileNameArr = dir.list();
 
-      sb.append("### logfile uploaded to s3 : ").append(LocalDateTime.now());
+      for(String logFileName : fileNameArr){
+        if(logFileName.endsWith(".txt")){
+          File s3UploadTargetFile = new File("./vr3_logs/" + logFileName);
 
-      amazonS3Client.putObject(
-          bucketName, "test-log-file.log", sb.toString()
-      );
+          amazonS3Client.putObject(bucketName, logFileName, s3UploadTargetFile);
 
-      // 컨테이너에서는 로그를 삭제한다
-      File uploadedLogFile = new File("./vr3_logs/vr3.log");
-      boolean logfileDeleteResult = uploadedLogFile.delete();
-      log.error("### uploadLogFile delete result : " + logfileDeleteResult);
+          boolean deleteResult = s3UploadTargetFile.delete();
+
+          log.info("### uploadLogFile delete result : " + deleteResult);
+        }
+      }//for
 
     } catch (Exception e) {
       log.error("### uploadLogFile : exception occurred while uploading log file.");
